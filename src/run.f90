cf4328501bd2109f90c49902fa19248272939311
whitaker.jeffrey <whitaker.jeffrey@f20321d9-1510-55bf-fa9e-3cc32f6ee687>
2012-05-10 18:27:41 +0000

    don't use 1.d0

--- src/run.f90
+++ src/run.f90
@@ -35,11 +35,11 @@ subroutine run()
   do nt=1,ntmax
      t = tstart + nt*dt
      call system_clock(count_0, count_rate, count_max)
-     t1 = count_0*1.d0/count_rate
+     t1 = count_0*1.0/count_rate
      ! advance solution with RK3
      call advance()
      call system_clock(count_1, count_rate, count_max)
-     t2 = count_1*1.d0/count_rate
+     t2 = count_1*1.0/count_rate
      spd = sqrt(ug**2+vg**2) ! max wind speed
      ! write out data at specified intervals.
      if (ntout .ne. 0 .and. mod(nt,ntout) .eq. 0) then
