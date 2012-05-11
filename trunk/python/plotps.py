2eb286b999813db318cc8a5c54405d19168f0021
whitaker.jeffrey <whitaker.jeffrey@f20321d9-1510-55bf-fa9e-3cc32f6ee687>
2012-05-11 15:45:54 +0000

    update

--- trunk/python/plotps.py
+++ trunk/python/plotps.py
@@ -1,7 +1,7 @@
 from read_sigma import read_header, read_specdata, read_griddata
 import spharm
 import numpy as np
-from mpl_toolkits.basemap import Basemap
+from mpl_toolkits.basemap import Basemap, addcyclic
 import matplotlib.pyplot as plt
 import sys
 
@@ -24,23 +24,34 @@ class ncepsigma(object):
         read_specdata(self.filename,self.ntrunc,self.nlevs)
         return vrtspec.T,divspec.T,tempspec.T,zspec,lnpsspec,qspec.T
 
-filename = sys.argv[1]
-
-sigfile = ncepsigma(filename)
-vrtspec,divspec,tempspec,zspec,lnpsspec,qspec = sigfile.specdata()
-
-lons,lats = np.meshgrid(sigfile.lons,sigfile.lats)
-psg = sigfile.spectogrd(lnpsspec)
-psg = 10.*np.exp(psg) # hPa
-print psg.min(), psg.max(), psg.shape
-
+#filename = sys.argv[1]
 m = Basemap(projection='npstere',boundinglat=15,lon_0=90,round=True)
-x,y = m(lons,lats)
-levs = np.arange(850,1050,5).tolist()
-levs.remove(1000)
-m.contour(x,y,psg,[1000.],linestyles='dotted',linewidths=1,colors='k')
-m.contour(x,y,psg,levs,linewidths=1,colors='k')
-m.drawparallels(np.arange(0,76,15),latmax=75)
-m.drawmeridians(np.arange(0,360,30),labels=[1,1,1,1],latmax=90)
-plt.title('ps from %s'%filename,y=1.05)
+fig = plt.figure(figsize=(15,5))
+npanel = 1
+for fhour in [252,336,384]:
+    filename = 'sig.f%s' % fhour
+
+    sigfile = ncepsigma(filename)
+    vrtspec,divspec,tempspec,zspec,lnpsspec,qspec = sigfile.specdata()
+    
+    lons,lats = np.meshgrid(sigfile.lons,sigfile.lats)
+    psg = sigfile.spectogrd(lnpsspec)
+    psg = 10.*np.exp(psg) # hPa
+    print psg.min(), psg.max(), psg.shape
+    
+    # add wraparound (cyclic) points.
+    lons1 = lons[0,:]
+    lats1 = lats[:,0]
+    psg, lons1 = addcyclic(psg, lons1)
+    lons, lats = np.meshgrid(lons1,lats1)
+    x,y = m(lons,lats)
+    levs = np.arange(850,1050,5).tolist()
+    levs.remove(1000)
+    ax = fig.add_subplot(1,3,npanel)
+    m.contour(x,y,psg,[1000.],linestyles='dotted',linewidths=1,colors='k')
+    m.contour(x,y,psg,levs,linewidths=1,colors='k')
+    m.drawparallels(np.arange(0,76,15),latmax=75)
+    m.drawmeridians(np.arange(0,360,30),labels=[1,1,1,1],latmax=90)
+    plt.title('hour %s'%fhour,y=1.05)
+    npanel = npanel + 1
 plt.show()
