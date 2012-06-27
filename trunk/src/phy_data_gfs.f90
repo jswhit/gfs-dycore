a144b0760a16d0c92dfe002fe4a43f8bf8df11d2
whitaker.jeffrey <whitaker.jeffrey@f20321d9-1510-55bf-fa9e-3cc32f6ee687>
2012-06-27 16:12:53 +0000

    initialize cv,cvt,cvb zero, remove un-needed files

--- trunk/src/phy_data_gfs.f90
+++ trunk/src/phy_data_gfs.f90
@@ -420,6 +420,9 @@ module phy_data
    cvb = data%cvb
    allocate(cvt(nlons,nlats))
    cvt = data%cvt
+   ! not needed anyway (only used for diagnostic clouds).
+   ! file contains missing values
+   cv = 0; cvb = 0; cvt = 0;
    allocate(alvsf(nlons,nlats))
    alvsf = data%alvsf
    allocate(alvwf(nlons,nlats))
@@ -1094,9 +1097,9 @@ module phy_data
    data%snoalb=snoalb
    data%orog=oro
 ! Not needed for version 200501
-!  data%cv=cv
-!  data%cvb=cvb
-!  data%cvt=cvt
+   data%cv=cv
+   data%cvb=cvb
+   data%cvt=cvt
    call sfcio_swohdc(lu,filename,head,data,iret)
    call sfcio_axdata(data,iret)
  end subroutine wrtout_sfc
