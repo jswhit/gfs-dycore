7ff53a1600f774f45980b659595cf5c93032d769
whitaker.jeffrey <whitaker.jeffrey@f20321d9-1510-55bf-fa9e-3cc32f6ee687>
2012-07-17 23:23:18 +0000

    fix error in previous commit

--- trunk/src/phy_run_gfs.f90
+++ trunk/src/phy_run_gfs.f90
@@ -211,7 +211,7 @@
    call astronomy                                                    &
 !  ---  inputs:
        ( nlons, nlats, lons, lats,                                   &
-         dtswr*3600., jdat, lsswr,                                   &
+         dtswr/3600., jdat, lsswr,                                   &
 !  ---  outputs:
          solcon, slag, sdec, cdec, coszen, coszdg                    &
         )
