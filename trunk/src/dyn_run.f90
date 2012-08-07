a1eb95e9ffad43f4d1c2bc2930bbf62c87f876da
whitaker.jeffrey <whitaker.jeffrey@f20321d9-1510-55bf-fa9e-3cc32f6ee687>
2012-08-07 17:31:26 +0000

    don't clobber module variable named eps

--- trunk/src/dyn_run.f90
+++ trunk/src/dyn_run.f90
@@ -461,7 +461,7 @@
    ! local variables 
    real(r_kind), dimension(:,:,:), allocatable :: datag_half, datag_d
    integer i,j,k
-   real(r_kind) phi(nlons,nlats),eps
+   real(r_kind) phi(nlons,nlats),epsilon
 
    allocate(datag_half(nlons,nlats,0:nlevs))
    allocate(datag_d(nlons,nlats,0:nlevs))
@@ -494,13 +494,13 @@
 !$omp end parallel do 
 
    ! to prevent NaNs in computation of van leer limiter
-   eps = tiny(phi(1,1))
+   epsilon = tiny(phi(1,1))
 !$omp parallel do private(i,j,k)
    do k=1,nlevs-1
       do j=1,nlats
       do i=1,nlons
-         if (abs(datag_d(i,j,k)) .lt. eps) then
-            datag_d(i,j,k) = sign(eps, datag_d(i,j,k))
+         if (abs(datag_d(i,j,k)) .lt. epsilon) then
+            datag_d(i,j,k) = sign(epsilon, datag_d(i,j,k))
          endif
       enddo
       enddo
