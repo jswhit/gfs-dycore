e529d408cb51cd0c7f3ecabad229148b0f752f72
whitaker.jeffrey <whitaker.jeffrey@f20321d9-1510-55bf-fa9e-3cc32f6ee687>
2012-12-18 18:08:33 +0000

    remove iau branch

--- branches/iau/python/build_read_sigma.sh
+++
@@ -1,6 +0,0 @@
-gfortran -O2 -fPIC -c ../src/shtns.f90
-gfortran -O2 -fPIC -c ../src/kinds.f90
-gfortran -O2 -fPIC -c ../src/sigio_module.f90
-SHTNSDIR=/Users/${USER}
-FFTWDIR=/opt/local
-f2py -c read_sigma.f90 -m read_sigma --fcompiler=gnu95 shtns.o kinds.o sigio_module.o -L${SHTNSDIR}/lib -lshtns -L${FFTWDIR}/lib -lfftw3
--- branches/iau/src/libgfs/mpi_quit.f90
+++
@@ -1,5 +0,0 @@
-subroutine mpi_quit(ierr)
-  integer, intent(in) :: ierr
-  print *,'stopping with error code,ierr'
-  stop 
-end subroutine mpi_quit
